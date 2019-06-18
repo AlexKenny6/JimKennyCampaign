7ac85f8e3669e6000f6f0f234e81815d7bd9978b
AlexKenny6 <alexkenny27@gmail.com>
2019-06-18 12:05:00 +0100

    Footer.php, currently being used as of 6/18/19.

---
+++ Code/Resources/templates/front/footer.php
@@ -0,0 +1,43 @@
+        <div class="footer w3-red">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <ul class="footer_box">
+                            <h4>Background</h4>
+                            <li><a href="background.php">Jim's Background</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <ul class="footer_box">
+                            <h4>Policies</h4>
+                            <li><a href="policies.php">Jim's Policies</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <ul class="footer_box">
+                            <h4>Contact</h4>
+                            <li><a href="contact.php">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <ul class="footer_box">
+                            <h4>Admin Login</h4>
+                            <li><a href="login.php">Log In</a></li>
+                        </ul>
+                    </div>
+                    <br><br>
+                    <div class="row footer_bottom">
+                        <div class="copy">
+                            <p> &copy; 2019, Jim Kenny for Fire Commissioner, All rights reserved.
+                            <br>Designed and Developed by <a href="https://pacwestsoftware.com/index.html" 
+                            data-size="large" target="_blank">&copy; Pacwest Software Development LLC</a>, All rights reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+        <script src="js/smoothproducts.min.js"></script>
+    </body>	
+</html>
